0a448a91db9729c426ca8376d9b7ce5a063e47f8
walker <2398833647@qq.com>
2023-08-24 22:58:55 +0800

    finish Question 6: Softmax regression in C++

--- src/simple_ml_ext.cpp
+++ src/simple_ml_ext.cpp
@@ -34,7 +34,79 @@ void softmax_regression_epoch_cpp(const float *X, const unsigned char *y,
 
     /// BEGIN YOUR CODE
 
-    /// END YOUR CODE
+    // 将真实标签转换为独热向量，然后再拼接成矩阵
+    float* Iy = new float[m * k];
+    for (size_t i = 0; i < m; i ++ ) {
+        for (size_t j = 0; j < k; j ++ ) {
+            if (y[i] == j) {
+                Iy[i * k + j] = 1.0;
+            } else {
+                Iy[i * k + j] = 0.0;
+            }
+        }
+    }
+
+    for (size_t i = 0; i < m; i += batch) {
+        // 创建X_batch和Iy_batch的指针，并进行内存分配
+        float* X_batch = new float[batch * n];
+        float* Iy_batch = new float[batch * k];
+
+        // 从X和Iy数组中拷贝数据到X_batch和Iy_batch中
+        memcpy(X_batch, X + i * n, batch * n * sizeof(float));
+        memcpy(Iy_batch, Iy + i * k, batch * k * sizeof(float));
+
+        // X * theta
+        float* X_theta = new float[batch * k];
+        for (size_t j = 0; j < batch; j ++ ) {
+            for (size_t u = 0; u < k; u ++ ) {
+                X_theta[j * k + u] = 0;
+                for (size_t v = 0; v < n; v ++ ) {
+                    X_theta[j * k + u] += X_batch[j * n + v] * theta[v * k + u];
+                }
+            }
+        }
+
+        // normalize(exp(X_theta))
+        float* exps = new float[batch * k];
+        float* Z = new float[batch * k];
+        for (size_t j = 0; j < batch; j ++ ) {
+            float sum_exp = 0;
+            for (size_t u = 0; u < k; u ++ ) {
+                exps[j * k + u] = exp(X_theta[j * k + u]);
+                sum_exp += exps[j * k + u];
+            }
+
+            for (size_t u = 0; u < k; u ++ ) {
+                Z[j * k + u] = exps[j * k + u] / sum_exp;
+            }
+        }
+
+        // 计算梯度并更新theta
+        float* dTheta = new float[n * k];
+        std::memset(dTheta, 0, n * k * sizeof(float));
+
+        for (size_t j = 0; j < batch; j ++ ) {
+            for (size_t u = 0; u < k; u ++ ) {
+                for (size_t v = 0; v < n; v ++ ) {
+                    dTheta[v * k + u] += X_batch[j * n + v] * (Z[j * k + u] - Iy_batch[j * k + u]);
+                }
+            }
+        }
+
+        for (size_t j = 0; j < n * k; j ++ ) {
+            theta[j] -= lr * dTheta[j] / batch;
+        }
+
+        // 释放内存
+        delete[] X_batch;
+        delete[] Iy_batch;
+        delete[] X_theta;
+        delete[] exps;
+        delete[] Z;
+        delete[] dTheta;
+    }
+    // 释放内存
+    delete[] Iy;    /// END YOUR CODE
 }
 
 
